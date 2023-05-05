ef7118502a717f88039213373941bcde504ce681
Shivang <shivangmangal@gmail.com>
2023-05-05 16:41:22 +0200

    serverConfig

--- src/main/java/ConfigServlet.java
+++ src/main/java/ConfigServlet.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -8,15 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class ContextServlet extends HttpServlet{
+public class ConfigServlet extends HttpServlet{
 
 	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
 		
 		PrintWriter out = res.getWriter();
 		
-		ServletContext ctx = getServletContext();
+		ServletConfig cnf = getServletConfig();
 		
-		out.print("My name is: "+ctx.getInitParameter("name"));
+		out.print("My name is: "+cnf.getInitParameter("name"));
 	}
 }
 
